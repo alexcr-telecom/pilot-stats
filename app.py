d51a43f78dff56f7d5b97740e4f731b56ed5c2c2
Alexcr <alexcr.telecom@gmail.com>
2017-11-07 12:55:28 +0300

    Commit

---
+++ app.py
@@ -0,0 +1,501 @@
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2015-2017 Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
+#
+
+from flask import Flask, render_template, jsonify, redirect, \
+    request, session, url_for
+from werkzeug.contrib.fixers import ProxyFix
+from werkzeug.serving import run_simple
+from werkzeug.wsgi import DispatcherMiddleware
+from werkzeug.exceptions import abort
+import logging
+
+from flask_babel import Babel, gettext
+import flask_login
+
+from qpanel import upgrader, job, rq_worker
+import qpanel.utils as uqpanel
+
+from qpanel.config import QPanelConfig
+from qpanel.backend import Backend
+from qpanel.database import session_db
+
+if QPanelConfig().has_queuelog_config():
+    from qpanel.model import queuelog_data_queue
+
+
+class User(flask_login.UserMixin):
+    pass
+
+
+cfg = QPanelConfig()
+backend = Backend()
+
+
+def get_data_queues(queue=None):
+    data = backend.get_data_queues()
+    if queue is not None:
+        try:
+            data = data[queue]
+        except:
+            abort(404)
+    if cfg.is_debug:
+        # app.logger.debug(data)
+        pass
+    return data
+
+
+def get_user_config_by_name(username):
+    try:
+        user = User()
+        user.id = username
+        user.password = cfg.get('users', username)
+        return user
+    except:
+        return None
+
+
+# Flask env
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.secret_key = cfg.secret_key
+babel = Babel(app)
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+app.config['BABEL_DEFAULT_LOCALE'] = cfg.language
+app.config['CONTEXT_IN'] = cfg.context_in
+app.config['CONTEXT_OUT'] = cfg.context_out
+app.config['SHOW_AGENTS'] = cfg.show_agents
+
+
+def set_data_user(user_config):
+    user = User()
+    user.id = user_config.id
+    return user
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return redirect(url_for('login'))
+
+
+@login_manager.user_loader
+def user_loader(username):
+    user_config = get_user_config_by_name(username)
+    if user_config is None:
+        return
+    return set_data_user(user_config)
+
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+    user_config = get_user_config_by_name(username)
+
+    if not cfg.has_users():
+        # fake login
+        user = User()
+        user.id = 'withoutlogin'
+        return user
+
+    if user_config is None:
+        return
+
+    user = set_data_user(user_config)
+    user.is_authenticated = user_config == request.form['pw']
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if not cfg.has_users():
+        return redirect(url_for('home'))
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    username = request.form['username']
+    user_config = get_user_config_by_name(username)
+    if user_config is None:
+        return redirect(url_for('login'))
+
+    if user_config.password == request.form['pw']:
+        user = set_data_user(user_config)
+        flask_login.login_user(user)
+        return redirect(url_for('home'))
+    return redirect(url_for('login'))
+
+
+@app.before_first_request
+def setup_logging():
+    # issue https://github.com/benoitc/gunicorn/issues/379
+    if not app.debug:
+        app.logger.addHandler(logging.StreamHandler())
+        app.logger.setLevel(logging.INFO)
+
+
+# babel
+@babel.localeselector
+def get_locale():
+    langs = ['en', 'es', 'de', 'pt_BR', 'ru']
+    browser = request.accept_languages.best_match(langs)
+    try:
+        return session['language']
+    except KeyError:
+        session['language'] = browser
+        return browser
+
+
+# Utilities helpers
+@app.context_processor
+def utility_processor():
+    def str_status_agent(value):
+        try:
+            value = int(value)
+        except:
+            value = 0
+        unavailable = [0, 4, 5]
+        free = [1]
+        in_call = [10]
+
+        if value in unavailable:
+            return gettext('unavailable')
+        elif value in free:
+            return gettext('free')
+        elif value in in_call:
+            return gettext('in call')
+        else:
+            return gettext('busy')
+
+    return dict(str_status_agent=str_status_agent)
+
+
+@app.context_processor
+def utility_processor():
+    def request_interval():
+        return cfg.interval * 1000
+
+    return dict(request_interval=request_interval)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.context_processor
+def utility_processor():
+    def check_upgrade():
+        return cfg.check_upgrade
+
+    return dict(check_upgrade=check_upgrade)
+
+
+@app.context_processor
+def utility_processor():
+    def show_service_level():
+        return cfg.show_service_level
+
+    return dict(show_service_level=show_service_level)
+
+
+@app.context_processor
+def utility_processor():
+    def has_users():
+        return cfg.has_users()
+
+    return dict(has_users=has_users)
+
+
+@app.context_processor
+def utility_processor():
+    def clean_str_to_div_id(value):
+        return uqpanel.clean_str_to_div_id(value)
+
+    return dict(clean_str_to_div_id=clean_str_to_div_id)
+
+
+@app.context_processor
+def utility_processor():
+    def is_freeswitch():
+        return backend.is_freeswitch()
+
+    return dict(is_freeswitch=is_freeswitch)
+
+
+@app.context_processor
+def utility_processor():
+    def config():
+        return cfg
+
+    return dict(config=config)
+
+
+@app.context_processor
+def utility_processor():
+    def current_version():
+        return upgrader.get_current_version()
+
+    return dict(current_version=current_version)
+
+
+# ---------------------
+# ---- Routes ---------
+# ---------------------
+# home
+@app.route('/')
+@flask_login.login_required
+def home():
+    data = get_data_queues()
+    template = 'index.html'
+    if backend.is_freeswitch():
+        template = 'fs/index.html'
+
+    context = {
+        'queues': data
+    }
+    return render_template(template, **context)
+
+
+@app.route('/queue/<name>')
+@flask_login.login_required
+def queue(name=None):
+    template = 'queue.html'
+    return render_template(template, **{'name': name})
+
+
+@app.route('/all_queues')
+@flask_login.login_required
+def all_queues():
+    data = get_data_queues()
+    template = 'all_queues.html'
+    if backend.is_freeswitch():
+        abort(404)
+        # Not yet implement
+        # template = 'fs/all_queue.html'
+    return render_template(template, queues=data)
+
+
+@app.route('/queue/<name>.json')
+@flask_login.login_required
+def queue_json(name=None):
+    data = get_data_queues(name)
+    backend.connection.flush()
+    members = data['members']
+    calls_count = backend.connection.get_calls_count()
+    busy, free, unavailable = backend.connection.get_members(members)
+    context = {
+        'name': name,
+        'data': data,
+
+        # Отвеченные звонки
+        'answered': {
+            'day': {
+                'value': backend.connection.get_answered_count(queue=name, period='day', holdtime=None),
+                'avg': backend.connection.get_answered_avg(queue=name, period='day')
+            },
+            'month': {
+                'value': backend.connection.get_answered_count(queue=name, period='month', holdtime=None),
+                'avg': backend.connection.get_answered_avg(queue=name, period='month')
+            },
+        },
+        # Пропущенные звонки
+        'abandon': {
+            'day': {
+                'value': backend.connection.get_abandon_count(queue=name, period='day', holdtime=False),
+                'avg': backend.connection.get_abandon_avg(queue=name, period='day'),
+            },
+            'month': {
+                'value': backend.connection.get_abandon_count(queue=name, period='month', holdtime=False),
+                'avg': backend.connection.get_abandon_avg(queue=name, period='month')
+            }
+        },
+        # Исходящие звонки
+        'outgoing': {
+            'day': {
+                'value': backend.connection.get_outgoing_count(members, period='day'),
+                'avg': backend.connection.get_outgoing_avg(members, period='day')
+            },
+            'month': {
+                'value': backend.connection.get_outgoing_count(members, period='month'),
+                'avg': backend.connection.get_outgoing_avg(members, period='month')
+            }
+        },
+        # Количество занятых линий
+        'busy': len(busy),
+
+        # Количество свободных линий
+        'free': len(free),
+
+        # Текущие звонки
+        'current': backend.connection.get_in_call(members),
+
+        # Входящие звонки
+        'internal': backend.connection.get_calls_queue_count(queues={name: data}, context=cfg.context_in),
+        'trunk': backend.connection.get_calls_queue_count(queues={name: data}, context=cfg.context_out),
+
+        # Время ожидания
+        'holdtime': cfg.holdtime,
+
+        # SLA
+        'sla': {
+            'abandon': {
+                'day': backend.connection.get_sla_abandon(name, period='day', count=calls_count),
+                'month': backend.connection.get_sla_abandon(name, period='month', count=calls_count)
+            },
+            'holdtime': {
+                'day': backend.connection.get_sla_answered(name, period='day', count=calls_count),
+                'month': backend.connection.get_sla_answered(name, period='month', count=calls_count)
+            }
+        }
+    }
+    session_db.rollback()
+    return jsonify(**context)
+
+
+# data queue
+@app.route('/queues')
+@flask_login.login_required
+def queues():
+    data_queues = get_data_queues()
+    context = {
+        'data': data_queues,
+        'current': backend.connection.get_core_channels_count(),
+        'internal': backend.connection.get_core_channels_count(cfg.context_out),
+        'trunk': backend.connection.get_core_channels_count(cfg.context_in),
+        'calls_queue': backend.connection.get_calls_queue(data_queues)
+    }
+    return jsonify(**context)
+
+
+@app.route('/lang')
+@app.route('/lang/<language>')
+@flask_login.login_required
+def language(language=None):
+    session['language'] = language
+    return redirect(url_for('home'))
+
+
+@app.route('/check_new_version')
+@flask_login.login_required
+def check_new_version():
+    need_upgrade = False
+    try:
+        if upgrader.require_upgrade():
+            need_upgrade = True
+    except:
+        pass
+
+    return jsonify(
+        require_upgrade=need_upgrade,
+        current_version=upgrader.get_current_version(),
+        last_stable_version=upgrader.get_stable_version()
+    )
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/spy', methods=['POST'])
+@flask_login.login_required
+def spy():
+    channel = request.form['channel']
+    to_exten = request.form['to_exten']
+    r = backend.spy(channel, to_exten)
+    return jsonify(result=r)
+
+
+@app.route('/whisper', methods=['POST'])
+@flask_login.login_required
+def whisper():
+    channel = request.form['channel']
+    to_exten = request.form['to_exten']
+    r = backend.whisper(channel, to_exten)
+    return jsonify(result=r)
+
+
+@app.route('/barge', methods=['POST'])
+@flask_login.login_required
+def barge():
+    channel = request.form['channel']
+    to_exten = request.form['to_exten']
+    r = backend.barge(channel, to_exten)
+    return jsonify(result=r)
+
+
+@app.route('/hangup', methods=['POST'])
+@flask_login.login_required
+def hangup_call():
+    channel = request.form['channel']
+    r = backend.hangup(channel)
+    return jsonify(result=r)
+
+
+@app.route('/stats/<from_date>/<to_date>/<name>.json')
+def stats_json(name, from_date, to_date):
+    real_name = uqpanel.realname_queue_rename(name)
+    queue_values = queuelog_data_queue(from_date, to_date, None, real_name)
+    data = get_data_queues(name)
+    return jsonify(name=name, data=data, values=queue_values)
+
+
+@app.route('/stats', defaults={'name': None, 'from_date': uqpanel.init_day(),
+                               'to_date': uqpanel.end_day()})
+@app.route('/stats/<name>/<from_date>/<to_date>')
+def stats(name, from_date, to_date):
+    queues = get_data_queues()
+    if name is None:
+        name = uqpanel.first_data_dict(queues)
+    try:
+        data = queues[name]
+    except:
+        data = {}
+    return render_template('stats.html', data=data, queues=queues, name=name,
+                           from_date=from_date, to_date=to_date)
+
+
+@app.route('/remove_from_queue', methods=['POST'])
+@flask_login.login_required
+def remove_from_queue():
+    queue = request.form['queue']
+    agent = request.form['agent']
+    r = backend.remove_from_queue(agent, queue)
+    return jsonify(result=r)
+
+
+# ---------------------
+# ---- Main  ----------
+# ---------------------
+def main():
+    # Set reloader to False, bug present for imports
+    # Retain this as FIXME
+    # https://github.com/mitsuhiko/flask/issues/1246
+    reloader = False
+
+    if cfg.is_debug:
+        app.config['DEBUG'] = True
+        uqpanel.add_debug_toolbar(app)
+    else:
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+    if cfg.queues_for_reset_stats():
+        if job.check_connect_redis():
+            rq_worker.start_jobs()
+        else:
+            print("Error: There not connection to Redis")
+            print("       Reset stats will not work\n")
+
+    if cfg.base_url == '/':
+        app.run(host=cfg.host_bind, port=cfg.port_bind, use_reloader=reloader,
+                extra_files=[cfg.path_config_file])
+    else:
+        application = DispatcherMiddleware(Flask('dummy_app'), {
+            app.config['APPLICATION_ROOT']: app,
+        })
+        run_simple(cfg.host_bind, cfg.port_bind, application,
+                   use_reloader=reloader, extra_files=[cfg.path_config_file])
